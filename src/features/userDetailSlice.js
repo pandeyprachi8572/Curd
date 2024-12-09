c3e43ef594d6c3410b5fa4018328fa41a6b40647
Prachi Pandey <pandeyprachi8572@gmail.com>
2024-12-09 15:13:14 +0530

    first commit

---
+++ src/features/userDetailSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit" ;
+
+
+
+
+export const userDetails =createSlice({
+    name :"userDetails",
+    initialState: {
+        users: [],
+        loading :false ,
+        console.error();
+        
+    }
+})
