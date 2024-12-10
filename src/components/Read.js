0029d231df796885cf9218d03ec769ef23d83158
Prachi Pandey <pandeyprachi8572@gmail.com>
2024-12-10 15:39:54 +0530

    *

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { use, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { showUser } from '../features/userDetailSlice';
 const Read = () => {
@@ -7,19 +7,29 @@ const {users , loading} = useSelector((state)=>state.app)
 useEffect(()=>{
     dispatch(showUser())
 },[])
+
+if(loading) {
+    return (<h2> loading </h2>)
+}
+
+
 return (
 <div>
       <h1> All Data </h1> 
       <div>
-      <div className="card w-50  mx-auto">
+        { users && users.map((ele)=>(
+
+        
+      <div className="card w-50  mx-auto my-2">
 <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 className="card-title">{ele.name}</h5>
+    <h6 className="card-subtitle mb-2 text-muted">{ele.email}</h6>
+    <p className="card-text">{ele.age}</p>
     <a href="#" className="card-link">Card link</a>
     <a href="#" className="card-link">Another link</a>
 </div>
 </div>
+))}
 </div>
 </div>
   )
